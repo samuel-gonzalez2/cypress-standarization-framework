5d27030113dd49d31045db38c3382cbf7c498006
samuel-gonzalez2 <samuel.gonzalez@enroutesystems.com>
2022-07-31 23:47:15 -0500

    [CREATED] Command chaining

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -3,6 +3,9 @@
 describe('Test Contact us form', () => {
     it.only('Should be able to submit valid data to the contact us form', () => {
         cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+        cy.title().should('include', 'WebDriver | Contact Us');
+        cy.url().should('include', 'contactus');
         //cy.get('#contact-us').click({force:true});
         cy.get('[name="first_name"]').type('John');
         cy.get('[name="last_name"]').type('Doe');
@@ -18,5 +21,6 @@ describe('Test Contact us form', () => {
         cy.get('[name="last_name"]').type('Doe');
         cy.get('textarea.feedback-input').type('Some message');
         cy.get('[type="submit"]').click();
+        cy.get('body').contains('Error: all fields are required');
     });
 });
