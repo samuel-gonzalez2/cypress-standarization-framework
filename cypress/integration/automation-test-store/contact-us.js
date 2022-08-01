1c816573dfa29c46fff4b16754fa55ba81200a38
samuel-gonzalez2 <samuel.gonzalez@enroutesystems.com>
2022-07-31 21:05:32 -0500

    [CREATED]Mocha and Chai libraries. First assertions

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -4,9 +4,13 @@ describe('Test Contact us form via automation store', () => {
     it('Should be able to submit valid data to the contact us form', () => {
         cy.visit('https://www.automationteststore.com/');
         cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.xpath("/html//div[@id='footer']//section[@class='footerlinks']//ul[@class='info_links_footer']//a[@href='https://automationteststore.com/index.php?rt=content/contact']").click();
         cy.get('#ContactUsFrm_first_name').type('John');
         cy.get('#ContactUsFrm_email').type('asdf@asdf.com');
+        cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
         cy.get('#ContactUsFrm_enquiry').type('Some message');
         cy.get('.col-md-6 > .btn').click();
+
+        cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!');
     });
 });
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Test Contact us form', () => {
-    it('Should be able to submit valid data to the contact us form', () => {
+    it.only('Should be able to submit valid data to the contact us form', () => {
         cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html');
         //cy.get('#contact-us').click({force:true});
         cy.get('[name="first_name"]').type('John');
@@ -9,6 +9,7 @@ describe('Test Contact us form', () => {
         cy.get('[name="email"]').type('asdf@ghjk.com');
         cy.get('textarea.feedback-input').type('Some message');
         cy.get('[type="submit"]').click();
+        cy.get('h1').should('have.text', 'Thank You for your Message!');
     });
 
     it('Should not be able to submit invalid data to the contact us form', () => {
